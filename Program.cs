da7f70fa2e0d4ac8c1591e3185f11d5663ddb8d1
artur-kordzik-cd <artur.kordzik@currentdesk.com>
2017-11-27 12:06:22 +0100

    init console app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dealer = new Dealer();
+            dealer.Initialize();
+            dealer.Start("server", 0, "pass");
+
+            Console.Read();
+        }
+    }
+}
